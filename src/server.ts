dffdfcd681bec0d7af2b0b649d2c3ffc54ddcda4
maiws <maiara.sd@live.com>
2020-06-03 22:30:26 -0300

    Projeto inicial Ecoleta

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+// Rota: endereço completo da nossa requisição
+// Recurso: qual entidade estamos acessando do sistema
+
+// GET: buscar uma ou mais informação do backend
+// POST: criar uma nova informação no backend
+// PUT: atualizar uma informação existente do backend
+// DELETE: remover uma informação do backend
+
+//POST http://localhost:3333/users - Criar um usuário
+//GET http://localhost:3333/users - Listar um usuário
+//GET http://localhost:3333/users/5 - Buscar dados do usuário com id 5
+
+//Request Param: parâmetros que vem na própria rota que identificam um recurso
+// Query Param: parâmtros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: parâmetros para criação/atualização de informações
+
+//JSON
+
+
+
+
+app.listen(3333);
